3fdde31fdb383fce5cc42ec8a93cef03d2f2a7f3
Born2Ride <30387055+ccirelli2@users.noreply.github.com>
2022-03-09 14:52:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# prefect_sandbox
+learning how to use prefect to build python etl
---
+++ tests/tests.py
@@ -0,0 +1,8 @@
+"""
+Scratchpad
+"""
+
+
+def hello(text: str) -> None:
+    """print hello and string"""
+    print(f"Hello {text}")
